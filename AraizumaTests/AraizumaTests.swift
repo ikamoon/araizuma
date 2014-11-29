a92d8222d61e250b78cfc3940d395aa6ea0e412c
Kaori Ikada <ikaikasama@gmail.com>
2014-11-29 20:00:22 +0900

    Initial Commit

---
+++ AraizumaTests/AraizumaTests.swift
@@ -0,0 +1,36 @@
+//
+//  AraizumaTests.swift
+//  AraizumaTests
+//
+//  Created by ikamon on 2014/11/29.
+//  Copyright (c) 2014年 araizuma. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AraizumaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
