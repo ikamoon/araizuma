a930f40f2c984c10879a0f19b7c21d577f585991
Kaori Ikada <ikaikasama@gmail.com>
2014-11-29 20:27:42 +0900

    add tab viewControllers

---
+++ Araizuma/AZColumnViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AZColumnViewController.swift
+//  Araizuma
+//
+//  Created by ikamon on 2014/11/29.
+//  Copyright (c) 2014年 araizuma. All rights reserved.
+//
+
+import UIKit
+
+class AZColumnViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
